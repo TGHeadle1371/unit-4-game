988b3903ffb6295d62ef39c6e2fe4ac198ee319c
TGHeadle1371 <tgheadle@gmail.com>
2018-12-09 10:55:35 -0700

    Fixed JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,183 +1,185 @@
-// Global variables
-
-var player; //player object
-var defender; //defender object
-var charArray = []; //Holds game characters
-var playerSelected = false; //Boolean to activate the character
-var defenderSelected = false; //Activate defender
-var baseAttack = 0; //Sets base attack to 0
 
 
-//Character properties
+$(document).ready(function () {
 
-function Character(name, hp, ap, counter, pic) {
-    this.name = name;
-    this.healthPoints = hp;
-    this.attackPower = ap;
-    this.counterAttackPower = counter;
-    this.pic = pic;
-}
+    var survivor;
+    var zombieEasy;
+    var zombieMedium;
+    var zombieHard;
 
+    var characterSelection = [];
+    var character = null;
+    var defenders = [];
+    var defender = null;
 
-//Attack Power Multiplier
-Character.prototype.attack = function () {
-    this.attackPower += baseAttack;
-};
-
-//Perform Attack
-
-Character.prototype.attack = function (Obj) {
-    Obj.healthPoints -= this.attackPower;
-    $("#msg").html("You attacked " +
-        Obj.name + "for " + this.attackPower + " damage points.");
-    this.attack();
-};
-
-//Perform Counter Attack
-
-Character.prototype.counterAttack = function (Obj) {
-    Obj.healthPoints -= this.counterAttackPower;
-    $("#msg").append("<br>" + this.name + " counter attacked you for " + this.counterAttackPower + " damage points.");
-};
-
-//Initialize Characters
-
-function initCharacters() {
-    var survivor = new Character("Survivor", 120, 15, 1, "./assets/images/survivor.jpg");
-    var zombieEasy = new Character("Slow Zombie", 150, 30, 10, "./assets/images/zombieEasy.jpeg");
-    var zombieMedium = new Character("Aggressive Zombie", 170, 40, 20, "./assets/images/zombieMedium.jpeg");
-    var zombieHard = new Character("BIG SCARY ZOMBIE", 190, 50, 30, "./assets/images/zombieHard.jpeg");
-    charArray.push(survivor, zombieEasy, zombieMedium, zombieHard);
-}
-
-
-//Save attack value
-function setBaseAttack(Obj) {
-    baseAttack = Obj.attackPower;
-}
-
-//Check HP
-function isAlive(Obj) {
-    if (Obj.healthPoints > 0) {
-        return true;
-    }
-    return false;
-}
-
-
-//Check if WIN
-function isWinner() {
-    if (charArray.length == 0 && player.healthPoints > 0)
-        return true;
-    else return false;
-}
-
-// Create the character cards onscreen
-function characterCards(divID) {
-    $(divID).children().remove();
-    for (var i = 0; i < charArray.length; i++) {
-        $(divID).append("<div />");
-        $(divID + " div:last-child").addClass("card");
-        $(divID + " div:last-child").append("<img />");
-        $(divID + " img:last-child").attr("id", charArray[i].name);
-        $(divID + " img:last-child").attr("class", "card-img-top");
-        $(divID + " img:last-child").attr("src", charArray[i].pic);
-        $(divID + " img:last-child").attr("width", 150);
-        $(divID + " img:last-child").addClass("img-thumbnail");
-        $(divID + " div:last-child").append(charArray[i].name + "<br>");
-        $(divID + " div:last-child").append("HP: " + charArray[i].healthPoints);
-        $(divID + " idv:last-child").append();
-
-    }
-}
-
-// Move img to DIV
-function movePics(fromDivID, toDivID) {
-$(fromDivID).children().remove();
-for (var i = 0; i < charArray.length; i++) {
-    $(toDivID).append("<img />");
-    $(toDivID + " img:last-child").attr("id", charArray[i].name);
-    $(toDivID + " img:last-child").attr("src", charArray[i].pic);
-    $(toDivID + " img:last-child").attr("width", 150);
-    $(toDivID + " img:last-child").addClass("img-thumbnail");
-}
-}
-
-//Document On Click Event
-
-$(document).on("click", "img", function () {
-    //Chooses defender and removes it from charArray
-    if (playerSelected && !defenderSelected && (this.id != player.name)) {
-        for (var i = 0; i < charArray.length; i++) {
-            if (charArray[i].name === (this).id) {
-                defender = charArray[i]; //Picks defender
-                charArray.splice(i, 1); //Splits it from the array
-                defenderSelected = true; //Adds it to the defenderSelected variable
-                $("#msg").html("Click to Attack!");
-            }
+    function startGame() {
+        survivor = {
+            id: 0,
+            name: "Brad Pitt",
+            healthPoints: 120,
+            baseAttack: 10,
+            attackPower: 10,
+            counterAttackPower: 8,
+            img: "./assets/images/survivor.jpg"
         }
-        $("#defenderDiv").append(this); // appends the selected defender to the div 
-        $("#defenderDiv").append("<br>" + defender.name);
-        $("#defenderHealthDiv").append("HP: " + defender.healthPoints);
-    }
 
-    //Stores User Character in playerSelected and removes from charArray
-    if (!playerSelected) {
-        for (var j = 0; j < charArray.length; j++) {
-            if (charArray[j].name === (this).id) {
-                player = charArray[j]; //Sets player
-                //Could add audio
-                setBaseAttack(player);
-                charArray.splice(j, 1);
-                playerSelected = true;
-                movePics();
-                $("#msg").html("Pick a Zombie to crush!");
-            }
+        zombieEasy = {
+            id: 1,
+            name: "Slow Zombie",
+            healthPoints: 100,
+            baseAttack: 8,
+            attackPower: 8,
+            counterAttackPower: 5,
+            img: "./assets/images/zombieEasy.jpeg"
         }
-        movePics("#game", "#defendersLeftDiv");
-        $("#playerDiv").append(this); // appends the selected player to the div
-        $("#playerDiv").append(player.name);
-        $("#playerHealthDiv").append("HP: " + player.healthPoints);
-    }
-});
 
-// ATTACK BUTTON
+        zombieMedium = {
+            id: 2,
+            name: "Strong Zombie",
+            healthPoints: 150,
+            baseAttack: 9,
+            attackPower: 9,
+            counterAttackPower: 10,
+            img: "./assets/images/zombieMedium.jpeg"
+        }
 
-$(document).on("click", "#attackBtn", function () {
-    if (playerSelected && defenderSelected) { // Given both player and defender are selected
-        if (isAlive(player) && isAlive(defender)) { //And if both are alive
-            player.attack(defender); //Player attacks defender
-            defender.counterAttack(player); //defender counterAttack
-            $("#playerHealthDiv").html("HP: " + player.healthPoints); //Display health points
-            $("#defenderHealthDiv").html("HP: " + defender.healthPoints);
-            if (!isAlive(defender)) { //If the defender dies, display DEFEAT, next
-                $("#defenderHealthDiv").html("DEFETED!");
-                $("#playerHealthDiv").html("Enemy defeated!");
-                $("#msg").html("Pick another enemy to battle...");
-            }
-            if (!isAlive(player)) { // If player dies, display LOST, RESTART IF ATTACK CLICK
-                $("#playerHealthDiv").html("YOU LOST!");
-                $("#msg").html("Try again...");
-                $("#attackBtn").html("Restart Game");
-                $(document).on("click", "#attackBtn", function () { // restarts game
-                    location.reload(); //RELOAD
+        zombieHard = {
+            id: 3,
+            name: "Brute Zombie",
+            healthPoints: 100,
+            baseAttack: 12,
+            attackPower: 12,
+            counterAttackPower: 12,
+            img: "./assets/images/zombieHard.jpeg"
+        }
+        // reset character selected
+        character = null;
+
+        // reset enemies array
+        defenders = [];
+
+        // reset enemy selected
+        defender = null;
+
+        // reset character selections
+        characterSelection = [survivor, zombieEasy, zombieMedium, zombieHard];
+
+        // clears all character divs
+        $("#character").empty();
+        $("#defenderArea").empty();
+        $("#defender").empty();
+        $("#status").empty();
+
+        $.each(characterSelection, function (index, character) {
+            // create a div for each character to display character selection at start of the game
+            var newCharacterDiv = $("<div>").addClass("character panel panel-success").attr("id", character.id);
+
+            $("<div>").addClass("panel-heading").html(character.name).appendTo(newCharacterDiv);
+            $("<div>").addClass("panel-body").append("<img src='" + character.img + "'>").appendTo(newCharacterDiv);
+            $("<div>").addClass("panel-footer").append("<span class='hp'>" + character.healthPoints + "</span>").appendTo(newCharacterDiv);
+
+            // append new div to character selection
+            $("#characterSelection").append(newCharacterDiv);
+        });
+
+        $(".character").on("click", function () {
+            // when character has been selected
+            if (character === null) {
+                console.log("picked character");
+                //get id of character selected
+                var charId = parseInt($(this).attr("id"));
+
+                character = characterSelection[charId];
+
+                // loop through character array
+                $.each(characterSelection, function (index, character) {
+                    // add unselected characters to enemies array
+                    if (character.id !== charId) {
+                        defenders.push(character);
+                        $("#" + character.id).removeClass("character panel-success").addClass("defender panel-danger").appendTo("#defenderArea");
+                    } else {
+                        $("#" + character.id).appendTo("#character");
+                    }
+                });
+
+                // add click event after defender class has been added
+                $(".defender").on("click", function () {
+                    if (defender === null) {
+                        var defenderId = parseInt($(this).attr("id"));
+                        console.log(this);
+                        defender = characterSelection[defenderId];
+                        $("#" + defenderId).appendTo("#defender");
+                    }
                 });
             }
-        }
-        if (!isAlive(defender)) { //If defender is not alive, 
-            $("#defenderDiv").children().remove();
-            $("#defenderDiv").html("");
-            $("#defenderHealthDiv").html("");
-            defenderSelected = false;
-            if (isWinner()) {
-                $("#globalMsg").show();
-            }
-        }
+        });
+
+        $("#restart").hide();
     }
-});
-// EXECUTE
-$(document).ready(function () {
-    $("#globalMsg").hide();
-    initCharacters();
-    characterCards("#game");
+
+    startGame();
+
+
+
+    $("#attack").on("click", function () {
+        // when character has been selected, character has not been defeated and there are still defenders left
+        if (character !== null && character.healthPoints > 0 && defenders.length > 0) {
+            // created variable to store game status messages
+            var status = "";
+
+            // when defender has been selected
+            if (defender !== null) {
+                // decrease defender HP by character attack power
+                defender.healthPoints -= character.attackPower;
+                status += "You attacked " + defender.name + " for " + character.attackPower + " damage.<br>";
+
+                console.log("Defender: ", defender.name, defender.healthPoints);
+
+                // update defender HP
+                $("#" + defender.id + " .hp").html(defender.healthPoints);
+
+                // decrease character HP by defender counter attack power
+                character.healthPoints -= defender.counterAttackPower;
+                status += defender.name + " attacked you back for " + defender.counterAttackPower + " damage.<br>";
+
+                console.log("Character: ", character.name, character.healthPoints);
+
+                // update character HP
+                $("#" + character.id + " .hp").html(character.healthPoints);
+
+                // increase character attack power by base attack power
+                character.attackPower += character.baseAttack;
+
+                // when character is defeated
+                if (character.healthPoints <= 0) {
+                    status = "You've been defeated... GAME OVER!!!!";
+                    $("#restart").show();
+                } else if (defender.healthPoints <= 0) {	// when defender is defeated
+                    status = "You have defeated " + defender.name + ", you can choose to fight another enemy.";
+
+                    // clear defender selection
+                    $("#defender").empty();
+                    defender = null;
+
+                    // remove defeated defender from defender array
+                    defenders.splice(defenders.indexOf(defender), 1);
+                }
+
+                // when no defenders left
+                if (defenders.length === 0) {
+                    status = "You win!";
+                    $("#restart").show();
+                }
+            } else {
+                status = "No enemy here.";
+            }
+
+            $("#status").html(status);
+        }
+    })
+
+    $("#restart").on("click", function () {
+        startGame();
+    })
+
 });
